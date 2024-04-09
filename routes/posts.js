540073c6b2ba12a025ba41cf425532546904b6cf
Feederboy <mp509300@gmail.com>
2024-04-09 22:00:51 +0800

    modified:   routes/posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,26 @@ import db from '../scripts/db.js'
 const router = express.Router();
 
 
+async function queryPosts(postId){
+    const result = await db.query('select content, title from posts where id=$1', [postId]);
+    const content = result.rows[0].content;
+    const title = result.rows[0].title;
+    return {content, title};
+}
+
+async function queryReplies(postId){
+    const result = await db.query('select content, userid from replies where postid=$1', [postId]);
+    return result.rows;
+}
+async function queryUsersInfo(){
+    const result = await db.query('select * from users');
+    const usersInfo = result.rows.reduce((acc, {googleid, ...rest}) => {
+        acc[googleid] = rest;
+        return acc;
+    }, {});
+    return usersInfo;
+}
+
 router.get("/", async (req, res) => {
     const result = await db.query('SELECT * FROM posts');
     const posts = result.rows.map(({id, title, content})=>({id, title, content}));
@@ -12,12 +32,12 @@ router.get("/", async (req, res) => {
     res.render("posts.ejs",{posts: posts, activePage: 'posts', scripts: scripts});
 });
 
-router.get("/:id(\\d+)", async (req, res) => {
-    const id = req.params.id;
-    const result = await db.query('SELECT content, title FROM posts WHERE id=$1', [id]);
-    const content = result.rows[0].content;
-    const title = result.rows[0].title;
-    res.render("post.ejs", { title: title, content: content, activePage: 'posts'});
+router.get("/:postId(\\d+)", async (req, res) => {
+    const postId = req.params.postId;
+    const post = await queryPosts(postId);
+    const replies = await queryReplies(postId);
+    const usersInfo = await queryUsersInfo();
+    res.render("post.ejs", { post: post, replies: replies, usersInfo: usersInfo, activePage: 'posts'});
 });
 
 export default router;
